08180577f5ac06efe5b14444ff60e3d4b76df24b
Karl <Karl@DESKTOP-OT4GJSI>
2024-07-29 17:00:28 +0300

    Add project files.

---
+++ UUIDfuscation.c
@@ -0,0 +1,24 @@
+#include <Windows.h>
+#include <stdio.h>
+
+#pragma warning(disable:4996)
+
+// Function takes in 16 raw bytes and returns them in a UUID format
+
+char* GenerateUUid(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p) {
+
+	// Each UUID segment is 32 bytes (bits?)
+	char Output0[32], Output1[32], Output2[32], Output3[32];
+
+
+	// UUID itself consisting of the 4 segments that I declared below
+	char result[128]
+
+}
+
+
+int main(void) {
+
+
+	return 0;
+}
